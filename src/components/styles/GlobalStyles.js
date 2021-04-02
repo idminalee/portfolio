20f85620b723ccaf0191f583d84f4e0ed9ce5a29
Mina Lee <minaleedev@gmail.com>
2021-04-02 20:22:26 +0100

    Declare typography constants

--- src/components/styles/GlobalStyles.js
+++ src/components/styles/GlobalStyles.js
@@ -1,5 +1,6 @@
 import { createGlobalStyle } from 'styled-components'
 import { normalize } from 'styled-normalize'
+import { FONT_PRIMARY } from './constants/typography'
 
 const GlobalStyles = createGlobalStyle`
   ${normalize}
@@ -19,9 +20,8 @@ const GlobalStyles = createGlobalStyle`
   body {
     margin: 0;
     padding: 0;
-    font-family: 'Montserrat', -apple-system, BlinkMacSystemFont, 'Segoe UI', 'Roboto', 'Oxygen',
-      'Ubuntu', 'Cantarell', 'Fira Sans', 'Droid Sans', 'Helvetica Neue',
-      sans-serif;
+    font-family: ${FONT_PRIMARY};
+    line-height: 1;
     letter-spacing: 0.01em;
     -webkit-font-smoothing: antialiased;
     -moz-osx-font-smoothing: grayscale;
---
+++ src/components/styles/constants/typography.js
@@ -0,0 +1,15 @@
+export const FONT_PRIMARY = `'Montserrat', -apple-system, BlinkMacSystemFont, 'Segoe UI', 'Roboto', 'Oxygen', 'Ubuntu',
+  'Cantarell', 'Fira Sans', 'Droid Sans', 'Helvetica Neue', sans-serif`
+
+export const FONT_SECONDARY = `'Poppins', -apple-system, BlinkMacSystemFont, 'Segoe UI', 'Roboto', 'Oxygen', 'Ubuntu',
+  'Cantarell', 'Fira Sans', 'Droid Sans', 'Helvetica Neue', sans-serif`
+
+export const FONT_11 = '11px'
+export const FONT_12 = '12px'
+export const FONT_14 = '14px'
+export const FONT_16 = '16px'
+export const FONT_18 = '18px'
+export const FONT_20 = '20px'
+export const FONT_24 = '24px'
+export const FONT_30 = '30px'
+export const FONT_32 = '32px'
