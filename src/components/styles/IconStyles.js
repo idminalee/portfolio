00bcfb1a099874f0fd81200befb6259a296e78e9
Mina Lee <minaleedev@gmail.com>
2021-04-03 17:51:15 +0100

    Create IconStyles

---
+++ src/components/styles/IconStyles.js
@@ -0,0 +1,32 @@
+import styled from 'styled-components'
+import * as colour from './constants/colours'
+import { displayFlex } from './mixins/flexbox'
+
+export const IconContainer = styled.div`
+  ${displayFlex('inline-flex')};
+  width: 32px;
+  height: 32px;
+
+  a,
+  button {
+    width: 100%;
+    height: 100%;
+  }
+
+  .icon {
+    color: ${colour.BLUE};
+    transition: color ease-in-out 200ms;
+
+    &:hover {
+      color: ${colour.BLUE_DARK};
+    }
+  }
+
+  .dimmed {
+    color: ${colour.SECONDARY};
+
+    &:hover {
+      color: ${colour.BLUE};
+    }
+  }
+`
