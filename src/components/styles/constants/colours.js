a8d01b6c69506c4933279b1e2f86d37ed59a434f
Mina Lee <minaleedev@gmail.com>
2021-04-11 20:19:17 +0100

    Add a colour constant

--- src/components/styles/constants/colours.js
+++ src/components/styles/constants/colours.js
@@ -1,6 +1,7 @@
 export const PRIMARY = '#172A3A'
 export const SECONDARY = '#8C8D96'
 export const BACKGROUND = '#FCFFF9'
+export const BACKGROUND_DARK = '#EDF0EE'
 
 export const BLUE = '#66B6CC'
 export const BLUE_DARK = '#3D9DB8'
